4a4d57b053236cc862e3a21d085ea9d8bc8b0b04
Aditeya Shukla <aditeyashukla@gmail.com>
2019-07-08 20:56:40 -0700

    Improved style, added count function

--- Classify_routes.py
+++ Classify_routes.py
@@ -1,6 +1,6 @@
-#%% -*- coding: utf-8 -*-
+# %% -*- coding: utf-8 -*-
 """
-Created on Wednesday July 3, 2019
+Created on Wednesday July, 3 2019
 @authors: Aditeya S and Avi S
 """
 
@@ -8,17 +8,17 @@ import csv
 import json
 
 
-SIDBY_conds_SERFR = {'track': (340, 346), 'alt': (4000, 5501), 'ground_speed': (200, 275),
-                         'vrate': (-2000, -100), 'ground_distance': (0, 0.125), 'name': 'SERFR'}
+SIDBY_conds_SERFR = {'track': (340, 346), 'alt': (4000, 5501), 'ground_speed': (200, 275), 'vrate': (-2000, -100),
+                     'ground_distance': (0, 0.125), 'name': 'SERFR'}
 
-EDDY2_conds_SERFR = {'track': (340, 346), 'alt': (5700, 8401), 'ground_speed': (105, 401),
-                         'vrate': (-2053, -457), 'ground_distance': (0, 0.125), 'name': 'SERFR'}
+EDDY2_conds_SERFR = {'track': (340, 346), 'alt': (5700, 8401), 'ground_speed': (105, 401), 'vrate': (-2053, -457),
+                     'ground_distance': (0, 0.125), 'name': 'SERFR'}
 
-EDDY3_conds_SERFR = {'track': (340, 346), 'alt': (4500, 7400), 'ground_speed':(70, 265),
-                    'vrate':(-2176, 594), 'ground_distance': (0, 0.125), 'name': 'SERFR'}
+EDDY3_conds_SERFR = {'track': (340, 346), 'alt': (4500, 7400), 'ground_speed':(70, 265), 'vrate':(-2176, 594),
+                     'ground_distance': (0, 0.125), 'name': 'SERFR'}
 
-NARWL_conds_SERFR = {'track':(340, 346), 'alt':(7000, 10001), 'ground_speed':(196, 387),
-                    'vrate': (-2112, -181), 'ground_distance': (0, 0.125), 'name': 'SERFR'}
+NARWL_conds_SERFR = {'track':(340, 346), 'alt':(7000, 10001), 'ground_speed':(196, 387),'vrate': (-2112, -181),
+                     'ground_distance': (0, 0.125), 'name': 'SERFR'}
 
 SERFR = {
     "SIDBY_SFO_Approach_SE": SIDBY_conds_SERFR,
@@ -36,9 +36,7 @@ ALL_ROUTE_IDS = {
     "SERFR": []
 }
 
-target_date = input('Enter the target date (yymmdd): ')
-
-jsonFile = "FA_Sightings." + target_date + ".airport_ids.json.txt"
+target_date = ''
 
 
 # ------------------------------------------------------------------------------------
@@ -62,12 +60,15 @@ def apply_route_conditions(cond_set, row):
     vertical_rate = row[12]
     ground_speed = row[11]
 
-    if float(int(track)) in range(track_min, track_max) and int(altitude) in range(alt_min, alt_max) and \
-            int(vertical_rate) in range(vrate_min, vrate_max) and int(ground_speed) in range(ground_speed_min, ground_speed_max)\
-            and float(ground_distance) >= ground_distance_min and float(ground_distance) <= ground_distance_max:
+    if float(int(track)) in range(track_min, track_max) \
+            and int(altitude) in range(alt_min, alt_max) \
+            and int(vertical_rate) in range(vrate_min, vrate_max) \
+            and int(ground_speed) in range(ground_speed_min, ground_speed_max) \
+            and ground_distance_min <= float(ground_distance) <= ground_distance_max:
         return cond_set['name']
     else:
         return 'other'
+
 
 # ---------------------------------------------------------------------------------------
 # This function adds the route name to the row in the input file. It first finds the
@@ -76,6 +77,8 @@ def apply_route_conditions(cond_set, row):
 # the 'apply_route_conditions' returned 'True' to the file under the 'route' column/key.
 # ---------------------------------------------------------------------------------------
 def addToJSON(icaoList, routeName):
+    jsonFile = "FA_Sightings." + target_date + ".airport_ids.json.txt"
+    print("\nAdding to JSON feed...")
     with open(jsonFile, 'r+') as f:
 
         master_struct = json.load(f)
@@ -94,54 +97,10 @@ def addToJSON(icaoList, routeName):
                 elif 'route' not in seg_meta.keys():
                     seg_meta['route'] = 'other'
 
-                print(seg_data[0])
-                # flight_id = seg_meta['flight']  # Segment flight_id
-        f.seek(0)  # <--- should reset file position to the beginning.
+        f.seek(0)  # should reset file position to the beginning.
         json.dump(master_struct, f)
         f.truncate()
 
-# ----------------------------------------------------------------------------------------
-# This function opens the input file that is specified for each waypoint and target date.
-# It is the main function for the classification of routes. It calls the other functions
-# in order to complete the classifcation task that it is given.
-# ----------------------------------------------------------------------------------------
-def sightingReader():
-    for eachRoute in routes:
-        flight_ids = []
-        routeName = ' '
-        count = 0
-        for waypoint in eachRoute:
-
-            cond_set = eachRoute[waypoint]
-
-            file = "FA_rcas." + target_date + "." + waypoint + ".rca.txt"
-
-            print(file)
-            with open(file) as f_in:
-                reader = csv.reader(f_in, delimiter='\t', quotechar='"')
-
-                linecount = 0
-                # Transform the rest of the lines
-                for line in reader:
-                    if linecount < 3:
-                        print(line)
-                    if linecount == 3:
-                        line += {"Route"}
-                        print(line)
-                    if linecount > 3:
-                        routeName = cond_set['name']
-                        route = apply_route_conditions(cond_set, line)
-                        line += {route}
-                        if route != 'other':
-                            flight_ids.append(line[0])
-                            print(line)
-                    linecount += 1
-            count += 1
-
-        ALL_ROUTE_IDS[routeName] = flight_ids
-        print('FLIGHT IDS:\n', flight_ids)
-        print('ALL ROUTE IDS:\n', ALL_ROUTE_IDS)
-        listMaker()
 
 # ---------------------------------------------------------------------------------------
 # This function creates a dictionary which prints out the values to be the ICAO addresses
@@ -162,13 +121,56 @@ def listMaker():
                 id_list.add(icao_segment)
                 routeicaos[str(icao_name)] = id_list
             else:
-                id_list = []
-                id_list.append(icao_segment)
+                id_list = [icao_segment]
                 id_list = set(id_list)
                 routeicaos[str(icao_name)] = id_list
         addToJSON(routeicaos, str(eachRoute))
 
-    print(routeicaos)
+    print("\n\nROUTE TALLY:")
+    for eachRoute in ALL_ROUTE_IDS:
+        print(str(eachRoute) + ": " + str(len(ALL_ROUTE_IDS[eachRoute])))
 
 
-sightingReader()
+# ----------------------------------------------------------------------------------------
+# This function opens the input file that is specified for each waypoint and target date.
+# It is the main function for the classification of routes. It calls the other functions
+# in order to complete the classification task that it is given.
+# ----------------------------------------------------------------------------------------
+def sightingReader(date):
+    global target_date
+    target_date = date
+    for eachRoute in routes:
+        flight_ids = []
+        routeName = ' '
+        count = 0
+        for waypoint in eachRoute:
+
+            cond_set = eachRoute[waypoint]
+
+            file = "FA_rcas." + target_date + "." + waypoint + ".rca.txt"
+
+            with open(file) as f_in:
+                reader = csv.reader(f_in, delimiter='\t', quotechar='"')
+
+                linecount = 0
+                # Transform the rest of the lines
+                for line in reader:
+                    if linecount == 3:
+                        line += {"Route"}
+                    if linecount > 3:
+                        routeName = cond_set['name']
+                        route = apply_route_conditions(cond_set, line)
+                        line += {route}
+                        if route != 'other':
+                            flight_ids.append(line[0])
+                    linecount += 1
+            count += 1
+
+        ALL_ROUTE_IDS[routeName] = flight_ids
+        print('\nALL ROUTE IDS:\n', ALL_ROUTE_IDS)
+        listMaker()
+
+
+if __name__ == "__main__":
+    target_date_input = input('Enter the target date (yymmdd): ')
+    sightingReader(target_date_input)
